fa3edaa10020c02e575715d842dcb748cb2fcb3d
Селедка под шубой <91335979+StrastStudio@users.noreply.github.com>
2024-11-23 15:03:50 +0300

    dsadsads

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ from tkinter import *
 from pygame.math import Vector2
 vec2 = Vector2
 
-W =
-H =
-PlayerSize =
-Pos =
-PlayerColor =
-XFinish =
+W = 800
+H = 500
+PlayerSize = 100
+Pos = vec2(50, 50)
+PlayerColor = "red"
+XFinish = W - Pos.x
 
 def KeyHander(event):
-    pass
+    Canva.move(PlayerId, 10, 0)
 
 s = Tk()
 s.title("Догони меня если сможешь")
-Canva = Canvas(s, width=W, height=H, bg="width")
+Canva = Canvas(s, width=W, height=H, bg="white")
 Canva.pack()
-PlayerId =
-FinishId =
+PlayerId = Canva.create_rectangle(*Pos, Pos.x + PlayerSize, Pos.y + PlayerSize, fill=PlayerColor)
+FinishId = Canva.create_rectangle(XFinish, 0, XFinish + 10, H, fill="black")
 s.bind("<KeyRelease>", KeyHander)
 s.mainloop()
