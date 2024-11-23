24d7686408d81f9ed2a80f6b02417b506c69b6b3
Селедка под шубой <91335979+StrastStudio@users.noreply.github.com>
2024-11-23 14:57:05 +0300

    dsadsadsadsad

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from pygame.math import Vector2
+vec2 = Vector2
+
+W =
+H =
+PlayerSize =
+Pos =
+PlayerColor =
+XFinish =
+
+def KeyHander(event):
+    pass
+
+s = Tk()
+s.title("Догони меня если сможешь")
+Canva = Canvas(s, width=W, height=H, bg="width")
+Canva.pack()
+PlayerId =
+FinishId =
+s.bind("<KeyRelease>", KeyHander)
+s.mainloop()
